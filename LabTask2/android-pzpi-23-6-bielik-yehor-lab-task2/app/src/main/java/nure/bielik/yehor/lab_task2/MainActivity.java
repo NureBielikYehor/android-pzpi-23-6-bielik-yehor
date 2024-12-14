afc25fc896f8d0681b1fc0ad95e9c7b610c386cc
Yehor Bielik <yehor.bielik@nure.ua>
2024-12-14 16:17:43 +0200

    add pract-task3

---
+++ LabTask2/android-pzpi-23-6-bielik-yehor-lab-task2/app/src/main/java/nure/bielik/yehor/lab_task2/MainActivity.java
@@ -0,0 +1,89 @@
+package nure.bielik.yehor.lab_task2;
+
+import android.graphics.Color;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.SeekBar;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+    private final int[] red = new int[1];
+    private final int[] green = new int[1];
+    private final int[] blue = new int[1];
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        SeekBar redSeekBar = findViewById(R.id.red);
+        SeekBar greenSeekBar = findViewById(R.id.green);
+        SeekBar blueSeekBar = findViewById(R.id.blue);
+
+        redSeekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                red[0] = progress;
+                changeColor();
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+
+        greenSeekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                green[0] = progress;
+                changeColor();
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+
+        blueSeekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                blue[0] = progress;
+                changeColor();
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+    }
+
+    private void changeColor() {
+        View view = findViewById(R.id.panel);
+        int color = Color.rgb(red[0], green[0], blue[0]);
+        view.setBackgroundColor(color);
+    }
+}
